c7dd0bc9fd8c030b2efa7ccd818119fa02d6a6b2
McYoloSwag1337 <ephraime.aung@gmail.com>
2024-10-01 16:46:29 -0700

    Changes to be committed: 	new file:   test2.py

---
+++ test2.py
@@ -0,0 +1,13 @@
+#innformation
+name="Ephraim Aung"
+city="Gilroy"
+SSN=1234
+
+#Print individual
+print("Name=", name)
+print("City=",city)
+print("SSN=", SSN)
+
+#Print together
+print("I'm {}, I'm from {}, and my SSN is {}". format(name,city,SSN))
+
